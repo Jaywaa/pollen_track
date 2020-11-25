296f31668856426f3dbbdc7b179ff2b9932682e7
James <jamesbellairs@gmail.com>
2020-11-25 21:50:59 +0200

    remove cors

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,23 +1,20 @@
 import { https, logger } from 'firebase-functions';
 import { fetchPollenHtml } from './http/fetch-pollen-html';
 import { parsePollenHtml } from './parsers/parse-pollen-html';
-import * as cors from 'cors';
 
 // export const scheduledFunc = pubsub.schedule('* * * * *').onRun((context) => {
 //   logger.log('HELLO ITS ME THE SCHEDULED BOI');
 // });
 
 export const savePollenReport = https.onRequest(async (request, response) => {
-  cors({ origin: true })(request, response, async () => {
-    const start = new Date().getTime();
-    logger.info(`[BEGIN] - ${new Date().toISOString()}`);
+  const start = new Date().getTime();
+  logger.info(`[BEGIN] - ${new Date().toISOString()}`);
 
-    const html = await fetchPollenHtml();
-    const pollenData = await parsePollenHtml(html);
+  const html = await fetchPollenHtml();
+  const pollenData = await parsePollenHtml(html);
 
-    logger.info('Pollen data:', pollenData);
-    response.send(pollenData);
+  logger.info('Pollen data:', pollenData);
+  response.send(pollenData);
 
-    logger.info(`[END] ${new Date().getTime() - start}ms`);
-  });
+  logger.info(`[END] ${new Date().getTime() - start}ms`);
 });
